871fcb59a86a779b6f13249f70ca237d8dd6e6f1
Javier Cuenca Gento <80844803+jcuencagento@users.noreply.github.com>
2022-11-09 13:31:01 +0100

    Commit inicial

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
@@ -13,6 +14,6 @@ app.get('/', (req, res) => {
 
 
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT || 8080, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
